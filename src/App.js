6e7166ea1bf5f07271ebd07e9d8bf3097df9238f
SuhYPark <52521529+SuhYPark@users.noreply.github.com>
2020-02-18 18:12:03 -0500

    counter completed

--- src/App.js
+++ src/App.js
@@ -5,23 +5,121 @@ class Counter extends Component {
     super();
     this.state = {
       count: 0,
+      text: "Count by 1",
     }
   }
 
 //code goes here...methods
-increment = () => {
-  this.setState ({
-    count: this.state.count +1
-  })
+toggle = () => {
+  if(this.state.text == "Count by 1"){
+    this.setState({
+      text: "Count by 2"
+    });
+  }
+
+  else {
+    this.setState({
+      text: "Count by 1"
+    });
+  }
 }
+
+
+increment = () => {
+  if(this.state.text == "Count by 1"){
+      if(this.state.count >= 19){
+        this.setState({count: 20});
+      }
+      else{
+        this.setState({
+          count: this.state.count + 1,
+        });
+      }
+  }
+
+  else {
+    if(this.state.count >= 19){
+    this.setState({count: 20});
+    }
+    else{
+      this.setState({
+        count: this.state.count + 2,
+      });
+    }
+  }
+
+}
+
 
 decrement = () => {
-  this.setState ({
-    count: this.state.count -1
-  })
+  if(this.state.text == "Count by 1"){
+      if(this.state.count >= 1){
+        this.setState({
+          count: this.state.count - 1,
+        });
+      }
+      else{
+        this.setState({count: 0});
+      }
+  }
+
+  else {
+    if(this.state.count <= 1){
+    this.setState({count: 0});
+    }
+    else{
+      this.setState({
+        count: this.state.count - 2,
+      });
+    }
+  }
+
 }
 
 
+
+// increment = () => {
+//   if(this.state.text =="Count by 1"){
+//     if(this.state.count==20){
+//       this.setState({count: 20});
+//     }
+//
+//     else {
+//       this.setState({
+//         count: this.state.count + 1,
+//       });
+//     }}
+//   else {
+//     if(this.state.count==20){
+//       this.setState({count: 20});
+//     }
+//
+//     else {
+//       this.setState({
+//         count: this.state.count + 2,
+//       });
+//   }
+// }
+
+// 
+// decrement = () => {
+//     if(this.state.count==0){
+//       this.setState({count: 0});
+//     }
+//
+//     else {
+//       this.setState({
+//         count: this.state.count - 1,
+//       });
+//     }
+// }
+
+clear = () => {
+  this.setState ({
+    count: 0,
+
+  })
+}
 
   render() {
     return(
@@ -29,8 +127,10 @@ decrement = () => {
         <div className="navbar">Container</div>
         <div className="counter">
           <h1>{this.state.count}</h1>
+          <button type="button" onClick={this.toggle}>{this.state.text}</button>
           <button type="button" onClick={this.increment}>Increment</button>
           <button type="button" onClick={this.decrement}>Decrement</button>
+          <button type="button" onClick={this.clear}>Clear</button>
         </div>
       </div>
     )
